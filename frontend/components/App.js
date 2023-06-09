4077cd806e97f140bf9ae042c7ccc348a45ae4a2
babotavo419 <one.faith419@gmail.com>
2023-06-09 06:58:58 -0400

    todos from server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -28,8 +28,11 @@ class App extends React.Component {
         <div id="error">Error: No error hrer</div>
         <div id="todos">
           <h2>Todos:</h2>
-          <div>Walk the dog</div>
-          <div>Do laundry</div>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
         </div>
         <form id="todoForm">
           <input type="text" placeholder="Type todo"/>
