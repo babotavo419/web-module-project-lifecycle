e2bc7437fd2b748dcf728fc9e2dea0c28269f5b7
babotavo419 <one.faith419@gmail.com>
2023-06-10 06:00:28 -0400

    check off todos

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -51,6 +51,17 @@ class App extends React.Component {
       .catch(this.setAxiosResponseError)
   }
 
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({...this.state, todos: this.state.todos.map(td => {
+          if (td.id !== id) return td
+          return res.data.data
+        })})
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
   componentDidMount() {
     this.fetchAllTodos() 
   }
@@ -63,7 +74,9 @@ class App extends React.Component {
           <h2>Todos:</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
+              return <div onClick={this.toggleCompleted(td.id)}
+               key={td.id}>
+                {td.name} {td.completed ? ' ⭕': ''}</div>
             })
           }
         </div>
